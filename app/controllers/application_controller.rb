f184688cb43aa650aac7d204ef5ec0708d715520
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-03 16:31:21 +0530

    Create separate layouts for login screens and home screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
+  before_action :authenticate_user!
+	layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      'registration'
+    else
+      'application'
+    end
+  end
 end
