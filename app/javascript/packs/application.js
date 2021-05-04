723e25fea6ac7183389a83e6417551ce5aef5ee8
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-04 18:04:48 +0530

    Add `Datatable`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,28 @@ const images = require.context('../images', true)
 window.jQuery = $;
 window.$ = $;
 
+require("datatables.net")
+require('datatables.net-bs4')
+require("datatables.net-bs4/css/dataTables.bootstrap4.min.css")
+
+const dataTables = [];
+
+document.addEventListener("turbolinks:load", () => {
+  if (dataTables.length === 0 && $('.data-table').length !== 0) {
+    $('.data-table').each((_, element) => {
+      dataTables.push($(element).DataTable({
+        pageLength: 50
+      }));
+    });
+  }
+});
+
+document.addEventListener("turbolinks:before-cache", () => {
+  while (dataTables.length !== 0) {
+    dataTables.pop().destroy();
+  }
+});
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
