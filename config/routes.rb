4b12f15df048301e6a345dca0d95f3b970fe0113
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-04 12:24:26 +0530

    Change `root` to `Businesses#index`

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#index'
+  root to: 'businesses#index'
   # for user functionality
   devise_for :users, controllers: { 
   	registrations: 'users/registrations',
