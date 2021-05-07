37f1e7cdaf7e34362a1079c3fa2d3277a6cce6e0
prakashbacancy <prakash.chaudhary@bacancy.com>
2021-05-07 13:17:44 +0530

    added industry and type for business form

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,27 @@
 class Business < ApplicationRecord
   PERMITTED_PARAM = %w[id name industry business_type phone_no zip_code address city state
                        no_of_employee annual_revenue description domain].freeze
+  INDUSTRY = ["State and Local Government",
+	"Finance and insurance",
+	"Health/social care",
+	"Durable manufacturing",
+	"Retail trade",
+	"Wholesale trade",
+	"Non-durable manufacturing",
+	"Federal Government",
+	"Information",
+	"Arts, entertainment",
+	"Waste services",
+	"Other services",
+	"Utilities",
+	"Mining",
+	"Corporate management",
+	"Education services",
+	"Agriculture"]
+
+	TYPE = ["Prospect",
+	"Partner",
+	"Reseller",
+	"Vandor",
+	"Other"]
 end
