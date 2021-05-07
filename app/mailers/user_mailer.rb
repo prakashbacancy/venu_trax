b20aa17d7086a819cb5c170cda71bbd09c7b1df6
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-07 13:11:50 +0530

    Change related to `password`

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   def new_user_password_confirmation(user, raw)
     @raw = raw
     @user = user
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/javascript/images/login/VenuTRAX-WH-2s.png")
     mail(to: @user.email, subject: 'Welcome to the VenuTrax!')
   end
 end
