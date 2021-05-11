e1fd0dcaddfdce3f2cd3a6052140fd69f9108689
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-11 13:45:46 +0530

    Sort `Business::Industry`

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,27 +1,27 @@
 class Business < ApplicationRecord
   PERMITTED_PARAM = %w[id name industry business_type phone_no zip_code address city state
                        no_of_employee annual_revenue description domain].freeze
-  INDUSTRY = ["State and Local Government",
-	"Finance and insurance",
-	"Health/social care",
-	"Durable manufacturing",
-	"Retail trade",
-	"Wholesale trade",
-	"Non-durable manufacturing",
-	"Federal Government",
-	"Information",
-	"Arts, entertainment",
-	"Waste services",
-	"Other services",
-	"Utilities",
-	"Mining",
-	"Corporate management",
-	"Education services",
-	"Agriculture"]
+  INDUSTRY = ['State and Local Government',
+              'Finance and insurance',
+              'Health/social care',
+              'Durable manufacturing',
+              'Retail trade',
+              'Wholesale trade',
+              'Non-durable manufacturing',
+              'Federal Government',
+              'Information',
+              'Arts, entertainment',
+              'Waste services',
+              'Other services',
+              'Utilities',
+              'Mining',
+              'Corporate management',
+              'Education services',
+              'Agriculture'].sort
 
-	TYPE = ["Prospect",
-	"Partner",
-	"Reseller",
-	"Vandor",
-	"Other"]
+  TYPE = %w[Prospect
+            Partner
+            Reseller
+            Vandor
+            Other].freeze
 end
