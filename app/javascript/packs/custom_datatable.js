4fc69acdccb649dfdcec6c6a0123b44a21e2825c
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-17 08:57:55 +0530

    Prevent datatable to be reinitialized

--- app/javascript/packs/custom_datatable.js
+++ app/javascript/packs/custom_datatable.js
@@ -1,16 +1,22 @@
 window.custom_datatable = function(table){
-  let dataTable = table.DataTable({
-    dom: 't<"dataTable-bottom flex-md-row flex-column"lp>',
-    language: {
-      paginate: {
-        previous: "< Back",
-        next: "Next >"
+  let dataTable;
+  if($.fn.dataTable.isDataTable('#'+table.attr('id'))) {
+    dataTable = table.DataTable();
+  } else {
+    dataTable = table.DataTable({
+      dom: 't<"dataTable-bottom flex-md-row flex-column"lp>',
+      language: {
+        paginate: {
+          previous: "< Back",
+          next: "Next >"
+        },
+        lengthMenu: "_MENU_ Result per page"
       },
-      lengthMenu: "_MENU_ Result per page"
-    },
-    pageLength: 10
-  });
+      pageLength: 10
+    });
+  }
   $('.search_btn').on('click', function(){
     dataTable.search($('#searchbox').val()).draw();
   })
+
 }
