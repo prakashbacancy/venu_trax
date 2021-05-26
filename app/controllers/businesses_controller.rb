d61294e5c8b00c15c89f2a568042572dec0ac348
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-26 16:53:22 +0530

    Add `Add More` functionality in `Business` form

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,10 +1,10 @@
 class BusinessesController < ApplicationController
   before_action :authenticate_user!
-  before_action :business, only: %i[show new edit]
+  before_action :business, only: %i[show new edit create update]
   before_action :find_venues, only: %i[show]
   before_action :find_notes, only: %i[show]
-  before_action :set_klass, only: %i[show new edit]
-  before_action :find_dynamic_fields, only: %i[new edit]
+  before_action :set_klass, only: %i[show new edit create update]
+  before_action :find_dynamic_fields, only: %i[new edit create update]
 
   def index
     @businesses = Business.all
@@ -13,19 +13,21 @@ class BusinessesController < ApplicationController
   def create
     if business.update(business_params)
       flash[:success] = 'Business Successfully Added!'
+      @business = Business.new
     else
       flash[:alert] = 'Error Occurred While Adding A Business!'
     end
-    redirect_to businesses_path
+    @businesses = Business.all
   end
 
   def update
     if business.update(business_params)
       flash[:success] = 'Business Successfully Updated!'
+      @business = Business.new
     else
       flash[:alert] = 'Error Occurred While Updating A business!'
     end
-    redirect_to businesses_path
+    @businesses = Business.all
   end
 
   def destroy
