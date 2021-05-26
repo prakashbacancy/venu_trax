8c99972ab0df3c7cac8611107db001929e0e55a3
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-26 14:36:35 +0530

    Apply default scope to `note`

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,5 +3,7 @@ class Note < ApplicationRecord
   belongs_to :notable, polymorphic: true
   has_many :comments, as: :commentable
 
+  default_scope { order(created_at: :desc) }
+
   scope :recent, -> { order(created_at: :desc) }
 end
