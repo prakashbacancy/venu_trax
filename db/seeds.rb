5e299dd59a6a84c23b479412b73ba331d08eb0f0
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-05-31 17:02:54 +0530

    Update seeds for the venues' static fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,19 @@ user_prebuid_fields = {
 user_prebuid_fields.each do |k, v|
   Field.find_or_create_by(name: k, label: k, column_type: v, klass_id: Klass.user.id, custom: false, deletable: false, required: true, position: 0)
 end
+
+# To make existing Venue fields dynamic
+venue_prebuild_fields = {
+  'domain': 'URL',
+  'name': 'Text',
+  'phone_no': 'Phone',
+  'zip_code': 'Text',
+  'address': 'Text Area',
+  'city': 'Text',
+  'state': 'Text',
+  'description': 'Text Area',
+  'no_of_employee': 'Integer'
+}
+venue_prebuild_fields.each do |k, v|
+  Field.find_or_create_by(name: k, label: k, column_type: v, klass_id: Klass.venue.id, custom: false, deletable: false, required: true, position: 0)
+end
