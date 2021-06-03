810ade19479ebbc943b4a8dc87ee9084f714adfc
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-03 17:20:39 +0530

    Update `seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,8 +47,9 @@ venue_prebuild_fields.each do |k, v|
   Field.find_or_create_by(name: k, label: k, column_type: v, klass_id: Klass.venue.id, custom: false, deletable: false, required: true, position: 0)
 end
 
-# =========================================================================
-# NOTE: Run this manually once in your console after all migrations for `Dynamic Groups Fields`
+# # ===============================START===============================
+# # NOTE: Run this manually once in your console after all migrations for `Dynamic Groups Fields` (Required for the first time)
+# # ===================================================================
 #
 # Group.find_or_create_by(name: 'Business Details', label: 'Business Details', klass_id: Klass.business.id, default: true)
 # Group.find_or_create_by(name: 'Business Information', label: 'Business Information', klass_id: Klass.user.id, ancestry: Group.find_by(name: 'Business Details').id.to_s, default: true)
@@ -61,4 +62,14 @@ end
 # Group.find_or_create_by(name: 'Venue Details', label: 'Venue Details', klass_id: Klass.venue.id, default: true)
 # Group.find_or_create_by(name: 'Venue Information', label: 'Venue Information', klass_id: Klass.venue.id, ancestry: Group.find_by(name: 'Venue Details').id.to_s, default: true)
 # Klass.venue.fields.update_all(group_id: Group.find_by(name: 'Venue Information').id)
-# =========================================================================
+# # ===============================END=================================
+
+# # ===============================START===============================
+# # NOTE: Run this for creating just testing brands (Not required)
+# # ===================================================================
+#
+# Brand.create(name: 'Apple')
+# Brand.create(name: 'McDonals')
+# Brand.create(name: 'Microsoft')
+# Brand.create(name: 'Facebook')
+# # ===============================END=================================
