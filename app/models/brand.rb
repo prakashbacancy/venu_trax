705906a2e6108356231ea6565b4af7dfaf5f35c8
prakashbacancy <prakash.chaudhary@bacancy.com>
2021-06-08 16:11:19 +0530

    added brand changes

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,4 +2,5 @@ class Brand < ApplicationRecord
   has_many :event_brands, dependent: :destroy
   has_many :events, through: :event_brands
   has_many :revenue_sources, dependent: :destroy
+  PERMITTED_PARAMS = ["id", "name", "created_at", "updated_at", "domain_name", "phone_number", "website_url", "city", "state", "zip_code", "no_of_employee", "annual_revenue", "brand_owner", "country", "street_address", "description", "venue_id"]
 end
