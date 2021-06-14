14fb89e0ca048f8e690240f8226a76ca0fd83ef1
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-14 17:38:08 +0530

    Add `Profile Pic` for `VenueContact`

--- app/models/venue_contact.rb
+++ app/models/venue_contact.rb
@@ -2,6 +2,8 @@ class VenueContact < ApplicationRecord
   belongs_to :user
   belongs_to :venue
 
-  PERMITTED_PARAM = %w[id full_name phone_no email job_title user_id venue_id].freeze
+  has_one_attached :profile_pic, dependent: :destroy
+
+  PERMITTED_PARAM = %w[id full_name phone_no email job_title user_id venue_id profile_pic].freeze
   JOB_TITLE = %w[Developer Manager Other].freeze
 end
