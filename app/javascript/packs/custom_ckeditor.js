1b066e5172b992b2c49e34659c2eeb899652a915
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-14 16:36:03 +0530

    Fix the whole notes and meetings forms taking time to open up

--- app/javascript/packs/custom_ckeditor.js
+++ app/javascript/packs/custom_ckeditor.js
@@ -1,5 +1,7 @@
 window.custom_ckeditor = function(ckeditor_textbox_id){
-  CKEDITOR.plugins.addExternal( 'confighelper', 'https://martinezdelizarrondo.com/ckplugins/confighelper/' );
+  CKEDITOR.plugins.addExternal('confighelper','/ckeditor/plugins/confighelper/');
+
+  // CKEDITOR.plugins.addExternal( 'confighelper', 'https://martinezdelizarrondo.com/ckplugins/confighelper/' );
   CKEDITOR.on('instanceReady', function () {
     $('#' + ckeditor_textbox_id).attr('required', '');
     $.each(CKEDITOR.instances, function (instance) {
