1595f2a3c3da26490069ba254a633b48300756cc
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-15 16:26:35 +0530

    Make Dynamic Settings for Brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,6 +2,9 @@ class BrandsController < ApplicationController
   before_action :brand, only: %i[show edit update]
   before_action :load_brands, only: %i[create update]
   before_action :load_data, only: %i[show]
+  before_action :set_klass
+  before_action :find_dynamic_fields, only: %i[new edit create update]
+  before_action :find_basic_group, only: %i[show new edit create update]
 
 	def index
     @brands = Brand.all
@@ -50,7 +53,8 @@ class BrandsController < ApplicationController
   private
 
   def brand_params
-  	params.require(:brand).permit(Brand::PERMITTED_PARAMS)
+    dynamic_params = Klass.brand.fields.pluck(:name)
+    params.require(:brand).permit(Brand::PERMITTED_PARAMS + dynamic_params)
   end
 
   def redirect_url
@@ -78,5 +82,28 @@ class BrandsController < ApplicationController
   # Set new venue object for `Add More`
   def set_new_venue
     @brand = Brand.new
+  end
+
+  def set_klass
+    @klass = Klass.brand
+  end
+
+  def find_dynamic_fields
+    fields = @klass.fields.includes(:field_picklist_values)
+    @data = {}
+    fields.each do |field|
+      @data[field.name.to_sym] = field.field_picklist_values.pluck(:value)
+    end
+  end
+
+  def find_basic_group
+    @info_group = Group.brand_basic
+    @group = if params[:group_id].present?
+               group = Group.find_by(id: params[:group_id])
+               @root_group_id = group.root.id
+               group
+             else
+               Group.brand_basic
+             end
   end
 end
