6eccfeb2553f3131752b11f5ecf9f9de59aaecc7
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-17 09:37:09 +0530

    Refactor fetch attendees helper method

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,7 +1,11 @@
 module MeetingsHelper
   def all_attendees
-    { Users: current_user.all_without_contact_user.map { |u| [u.full_name, u.to_polymorphic] },
-      VenueContacts: @meeting.meetingable.venue_contacts.map { |u| [u.full_name, u.user.to_polymorphic] } }
+    attendees = {}
+    users = current_user.all_without_contact_user.map { |u| [u.full_name, u.to_polymorphic] }
+    venue_contacts = @meeting.meetingable.venue_contacts.map { |u| [u.full_name, u.user.to_polymorphic] }
+    attendees[:Users] = users if users.present?
+    attendees[:VenueContacts] = venue_contacts if venue_contacts.present?
+    attendees
   end
 
   def value_of_attendees_for_select(meeting)
