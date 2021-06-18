9a1dc403c1ab7ad886e5cbf352139ff4c624dc7d
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-18 14:09:33 +0530

    Refactor Dynamic Settings

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -8,10 +8,18 @@ class Klass < ApplicationRecord
   scope :event, -> { find_by(name: 'Event') }
   scope :brand, -> { find_by(name: 'Brand') }
 
-  validates :name, uniqueness: { case_sensitive: false, message: 'Entered klass name is already in use' }, allow_nil: false
+  validates :name, uniqueness: { case_sensitive: false, message: 'Entered klass name is already in use' },
+                   allow_nil: false
+
+  # Consider `ts_name` as `tableize_singularize_name`
+  DYNAMIC_KLASSES = [{ name: 'Business', ts_name: 'business' },
+                     { name: 'User', ts_name: 'user' },
+                     { name: 'Venue', ts_name: 'venue' },
+                     { name: 'Event', ts_name: 'event' },
+                     { name: 'Brand', ts_name: 'brand' }].freeze
 
   def constant
-    @constant ||= self.name.constantize
+    @constant ||= name.constantize
   end
 
   def field_picklist_valuable_fields
