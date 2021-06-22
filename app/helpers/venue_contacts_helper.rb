47fc7f23a075fe25e1743bbacdd6c1bbbd1dc383
Vatsal Mewada <vatsal.mewada@bacancy.com>
2021-06-22 13:14:23 +0530

    Add `Status` funtionality to `VenueContact`

--- app/helpers/venue_contacts_helper.rb
+++ app/helpers/venue_contacts_helper.rb
@@ -6,4 +6,17 @@ module VenueContactsHelper
   def no_data_available_for_table
     '<tr><td colspan="5" class="text-center text-gray">No Data Available</td></tr>'.html_safe
   end
+
+  def user_status_option(user_statuses)
+    user_statuses.collect { |user_status| [user_status.capitalize, user_status] }
+  end
+
+  def status_class(status)
+    status_hash = {
+      'active': 'bg-green',
+      'inactive': 'bg-red',
+      'invited': 'not-allowed bg-yellow'
+    }
+    status_hash[status]
+  end
 end
